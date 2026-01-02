ce3d8ff4938b90309bc937907d9b33b4ae237961
Kevin Xiao <xlzqwerty1@gmail.com>
2026-01-02 03:56:26 -0800

    Initial project setup

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tauri_tanstack_start_react_template_lib::run()
+}
